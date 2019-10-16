01ec212c1dce87108d09b6b36ca93670ba9b1260
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2019-10-16 22:14:56 +0200

    Add a new classifier

--- setup.py
+++ setup.py
@@ -8,7 +8,11 @@ setup(
     description=(),
     url="https://github.com/magmax/sphinx-jinyaml",
     license="MIT",
-    classifiers=["License :: OSI Approved :: MIT License"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Framework :: Sphinx :: Extension",
+    ],
     install_requires=["docutils>=0.11", "sphinx>=1.3.1"],
     extras_require={"markdown": ["commonmark>=0.8.1"]},
     packages=["jinyaml"],
